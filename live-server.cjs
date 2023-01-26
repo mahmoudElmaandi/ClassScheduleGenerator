b844c57e3411334c964ca7655a4f3b555053a0cc
AlMoaanediLabs <anrobot2020@gmail.com>
2023-01-27 00:55:25 +0200

    updating dependencies

--- live-server.cjs
+++ live-server.cjs
@@ -7,13 +7,13 @@ var params = {
     root: "dist", // Set root directory that's being served. Defaults to cwd.
     open: false, // When false, it won't load your browser by default.
     ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "index2.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
     mount: [['/components', './node_modules']], // Mount a directory to a route.
     logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
     middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
 };
-lookup(hostname(),
+lookup(hostname(), { family: 4 },
     function (err, add, fam) {
         // console.log('addr: ' + add);
         params.host = add
